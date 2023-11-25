798687185e37a3047fd58a892ae593f0a3e32bd6
2709patri <128839994+2709patri@users.noreply.github.com>
2023-11-25 00:58:33 -0300

    Update ContadorForm.jsx

--- src/ContadorForm.jsx
+++ src/ContadorForm.jsx
@@ -1,26 +1,40 @@
-import { useState } from 'react';
+// ContadorForm.js
+import React, { useState } from 'react';
 
 function ContadorForm({ alAgregar }) {
   const [nombre, setNombre] = useState('');
   const [marca, setMarca] = useState('');
+  const [cantidad, setCantidad] = useState(0);
+  const [unidad, setUnidad] = useState('unidad'); 
 
-  const handleNombreChange = event => {
+  const handleNombreChange = (event) => {
     setNombre(event.target.value);
   };
 
-  const handleMarcaChange = event => {
+  const handleMarcaChange = (event) => {
     setMarca(event.target.value);
   };
 
-  const agregar = evento => {
+  const handleCantidadChange = (event) => {
+    setCantidad(event.target.value);
+  };
+
+  const handleUnidadChange = (event) => {
+    setUnidad(event.target.value);
+  };
+
+  const agregar = (evento) => {
     if (nombre.trim() !== '') {
       alAgregar({
         nombre,
         marca,
-        cantidad: 0, // Agregamos cantidad con valor inicial 0 al agregar un nuevo ítem
+        cantidad,
+        unidad,
       });
       setNombre('');
       setMarca('');
+      setCantidad(0);
+      setUnidad('unidad');
     }
     evento.preventDefault();
   };
@@ -40,8 +54,24 @@ function ContadorForm({ alAgregar }) {
           type="text"
           value={marca}
           onChange={handleMarcaChange}
-          placeholder="Marca"
+          placeholder="Característica o Marca"
         />
+      </div>
+      <div>
+        <label>
+          Cantidad:
+          <input
+            type="number"
+            value={cantidad}
+            onChange={handleCantidadChange}
+            placeholder="Cantidad"
+          />
+        </label>
+        <select value={unidad} onChange={handleUnidadChange}>
+          <option value="unidad">Unidad</option>
+          <option value="kilo">Kg.</option>
+          <option value="litro">Lt.</option>
+        </select>
       </div>
       <button type="submit">Agregar</button>
     </form>
